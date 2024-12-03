8ee8e4f6fe84c0512f3a197b82a1581d608adf24
Jacob <jayman712016@gmail.com>
2024-12-02 21:23:14 -0400

    Second Commit

--- Lab5/Form1.cs
+++ Lab5/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//jacob chiasson, https://github.com/Seabeaver/ChiassonLab5
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -28,67 +29,120 @@ namespace Lab5
         private void Form1_Load(object sender, EventArgs e)
         {
             //select one roll radiobutton
-            
+            radOneRoll.Enabled = true;
+
             //add your name to end of form title
+            this.Text += "Jacob Chiasson";
             
         } // end form load
 
         private void btnClear_Click(object sender, EventArgs e)
         {
             //call the function
+            ClearOneRoll();
         }
 
         private void btnReset_Click(object sender, EventArgs e)
         {
             //call the function
-            
+            ClearStats();
         }
 
         private void btnRollDice_Click(object sender, EventArgs e)
         {
             int dice1, dice2;
             //call ftn RollDice, placing returned number into integers
-            
+            dice1 = RollDice();
+            dice2 = RollDice();
             //place integers into labels
-            
+            lblDice1.Text = dice1.ToString();
+            lblDice2.Text = dice2.ToString();
+
             // call ftn GetName sending total and returning name
+            int total = dice1 + dice2;
+            lblRollName.Text = GetName(total);
 
             //display name in label
-            
+
+        }
+        private void ClearOneRoll() {
+            /* Name: ClearOneRoll
+            *  Sent: nothing
+            *  Return: nothing
+            *  Clear the labels */
+            lblDice1.Text = "";
+            lblDice2.Text = "";
+            lblRollName.Text = "";
         }
 
-        /* Name: ClearOneRoll
-        *  Sent: nothing
-        *  Return: nothing
-        *  Clear the labels */
+        private void ClearStats() {
+            /* Name: ClearStats
+            *  Sent: nothing
+            *  Return: nothing
+            *  Reset nud to minimum value, chkbox unselected, 
+            *  clear labels and listbox */
+            nudNumber.Value = 50;
+            chkSeed.Enabled = false;
+            lstMarks.Items.Clear();
+            lblPass.Text = "";
+            lblFail.Text = "";
+            lblAverage.Text = "";
+        }
 
+        private void RollDice()
+        {
+            /* Name: RollDice
+            * Sent: nothing
+            * Return: integer (1-6)
+            * Simulates rolling one dice */
+        }
 
-        /* Name: ClearStats
-        *  Sent: nothing
-        *  Return: nothing
-        *  Reset nud to minimum value, chkbox unselected, 
-        *  clear labels and listbox */
+        private string GetName(int diceTotal)
+        {
+            /* Name: GetName
+            * Sent: 1 integer (total of dice1 and dice2) 
+            * Return: string (name associated with total) 
+            * Finds the name of dice roll based on total.
+            * Use a switch statement with one return only
+            * Names: 2 = Snake Eyes
+            *        3 = Litle Joe
+            *        5 = Fever
+            *        7 = Most Common
+            *        9 = Center Field
+            *        11 = Yo-leven
+            *        12 = Boxcars
+            * Anything else = No special name*/
 
-
-        /* Name: RollDice
-        * Sent: nothing
-        * Return: integer (1-6)
-        * Simulates rolling one dice */
-
-
-        /* Name: GetName
-        * Sent: 1 integer (total of dice1 and dice2) 
-        * Return: string (name associated with total) 
-        * Finds the name of dice roll based on total.
-        * Use a switch statement with one return only
-        * Names: 2 = Snake Eyes
-        *        3 = Litle Joe
-        *        5 = Fever
-        *        7 = Most Common
-        *        9 = Center Field
-        *        11 = Yo-leven
-        *        12 = Boxcars
-        * Anything else = No special name*/
+            string name = "";
+            switch (diceTotal)
+            {
+                case 2:
+                    name = "Snake Eyes";
+                    break;
+                case 3:
+                    name = "Litle Joe";
+                    break;
+                case 5:
+                    name = "Fever";
+                    break;
+                case 7:
+                    name = "Most Common";
+                    break;
+                case 9:
+                    name = "Center Field";
+                    break;
+                case 11:
+                    name = "Yo-leven";
+                    break;
+                case 12:
+                    name = "Boxcars";
+                    break;
+                default:
+                    name = "";
+                    break;
+            }
+            return name;
+        }
 
         private void btnSwapNumbers_Click(object sender, EventArgs e)
         {
